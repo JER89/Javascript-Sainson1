c576fe6990cd88fc7088c2facf952891c6affe9a
Jeremy Mertens <mertensjeremy89@gmail.com>
2021-02-08 09:58:30 +0100

    0907

---
+++ 09-misc/07-storage-clicker/script.js
@@ -0,0 +1,19 @@
+// 09-misc/07-storage-clicker/script.js - 9.7: storage clicker
+
+
+(() => {
+
+    document.getElementById("increment").addEventListener("click", () => {
+        if(localStorage.clicked) {
+            localStorage.clicked = Number(localStorage.clicked) + 1;
+        } else {
+            localStorage.clicked = 1;
+        }
+        display();
+    });
+    function display() {
+        document.getElementById('target').innerHTML = 
+        "You clicked" + localStorage.clicked + "time(s).";
+    }
+
+})();
